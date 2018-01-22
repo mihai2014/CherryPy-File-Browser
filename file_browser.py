893a30ec247e07524aeedeb24b96db6dc3f8392b
mihai2014 <mc200520052005@yahoo.com>
2018-01-22 15:03:48 +0200

    mobile friendly, python3

--- file_browser.py
+++ file_browser.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import cherrypy
 from jinja2 import Template
 
@@ -8,7 +9,7 @@ import platform
 def signal_handler(signal, frame):
         print('You pressed Ctrl+Z!')
 	#os.popen("fuser -k 8080/tcp")
-	sys.exit(0)
+        sys.exit(0)
 
 signal.signal(signal.SIGTSTP, signal_handler)
 
@@ -18,11 +19,11 @@ def order(dir):
     all_items = []
 
     for item in os.listdir(dir):
-	fullpath = os.path.join(dir, item)
-	if os.path.isdir(fullpath): 
-	    dirs.append(item)
-	else: 
-	    files.append(item)
+        fullpath = os.path.join(dir, item)
+        if os.path.isdir(fullpath): 
+            dirs.append(item)
+        else: 
+            files.append(item)
 
     return sorted(dirs) + sorted(files)
 
@@ -33,12 +34,12 @@ def ls(dir):
         fullpath = os.path.join(dir, item)
 
         if os.path.isdir(fullpath):
-	    data.append({'type':'D','href':'/change_directory/?path='+fullpath,'name':item,'info':''})
+            data.append({'type':'D','href':'/change_directory/?path='+fullpath,'name':item,'info':''})
         else:
             info = os.stat(fullpath)
-            #print os.path.getmtime(fullpath)
+            #print(os.path.getmtime(fullpath))
             describe = str(info.st_size) + " bytes"    #+ " " + str(info.st_atime) + " " + str(info.st_atime)
-	    data.append({'type':'F','href':'/static'+fullpath,'name':item,'info':describe})
+            data.append({'type':'F','href':'/static'+fullpath,'name':item,'info':describe})
 
     return data
 
@@ -55,12 +56,12 @@ def traverse1(dir):
 
         fullpath = os.path.join(dir, item)
 	
-	if os.path.isdir(fullpath):
-	    my_list += '<li>%s</li>' % item
-	    #data.append({'type':'D','href':'#','name':item})
-	else:
-	    my_list += '<li><a href="%s">%s</a></li>' % ('/static'+fullpath ,item)
-	    #data.append({'type':'F','href':'/static'+fullpath,'name':item})
+        if os.path.isdir(fullpath):
+            my_list += '<li>%s</li>' % item
+            #data.append({'type':'D','href':'#','name':item})
+        else:
+            my_list += '<li><a href="%s">%s</a></li>' % ('/static'+fullpath ,item)
+            #data.append({'type':'F','href':'/static'+fullpath,'name':item})
 
         if os.path.isdir(fullpath):
             if os.listdir(fullpath) != []:
@@ -77,11 +78,11 @@ def traverse2(rootDir):
 	#type = 'D'
 	#href = '#'
 	#name = '/static'+dirName+'/'
-	if len(fileList) == 0:
-	    data.append({'type':'D','href':'#','name':'/static'+dirName})
+        if len(fileList) == 0:
+            data.append({'type':'D','href':'#','name':'/static'+dirName})
         for fname in fileList:
-	    #my_list += '<li><a href="%s">%s</a></li>' % ('/static'+dirName+'/'+fname ,'/static'+dirName+'/'+fname)
-	    data.append({'type':'F','href':'/static'+dirName+'/'+fname,'name':dirName+'/'+fname})
+            #my_list += '<li><a href="%s">%s</a></li>' % ('/static'+dirName+'/'+fname ,'/static'+dirName+'/'+fname)
+            data.append({'type':'F','href':'/static'+dirName+'/'+fname,'name':dirName+'/'+fname})
     #my_list += '<ul>'
 
 
@@ -95,7 +96,7 @@ def get_data(dirName):
 
     if display_type == 'tree1':
         traverse1(dirName)
-	data = my_list
+        data = my_list
 
     if display_type == 'tree2':
         traverse2(dirName)
@@ -112,20 +113,20 @@ class root(object):
 
     @cherrypy.expose
     def index(self):
-	return get_data('/')
+        return get_data('/')
 
     @cherrypy.expose
     def change_directory(self, path):
-	return get_data(path)
+        return get_data(path)
 
     @cherrypy.expose
     def display(self, type):
-	global display_type
-	display_type = type
+        global display_type
+        display_type = type
 
 
 if __name__ == '__main__':
-    print "System: " + platform.system()
+    print ("System: " + platform.system())
 
     cherrypy.quickstart(root(),'/',"file_browser.conf")
 
